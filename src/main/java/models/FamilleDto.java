ce9b4a3e4173ac484293953b4a60256e1e99ec86
andilMc <pro.mchangama1998@gmail.com>
2025-04-14 06:28:30 +0000

    Setup Categories and Famillies

--- src/main/java/models/FamilleDto.java
+++ src/main/java/models/FamilleDto.java
@@ -1,7 +1,7 @@
-package models.DTO;
+package models;
 
 import jakarta.inject.Named;
-import models.Catalogue;
+import models.DTO.CatalogueDto;
 
 import javax.validation.constraints.Size;
 import java.io.Serializable;
@@ -9,57 +9,60 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 /**
- * DTO for {@link Catalogue}
+ * DTO for {@link Famille}
  */
 @Named
-public class CatalogueDto implements Serializable {
+public class FamilleDto implements Serializable {
     private  int id;
+    private  CatalogueDto idcatalogue;
     @Size(max = 254)
     private  String reference;
     @Size(max = 254)
     private  String label;
     @Size(max = 254)
     private  String description;
-    private  Boolean statut;
+    private  Boolean statut =true;
     private  LocalDate datecreation;
+
 
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
+    }
+    public CatalogueDto getIdcatalogue() {
+        return idcatalogue;
+    }
+    public void setIdcatalogue(CatalogueDto idcatalogue) {
+        this.idcatalogue = idcatalogue;
     }
 
     public String getReference() {
         return reference;
     }
-
     public void setReference(String reference) {
         this.reference = reference;
     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
     public String getLabel() {
         return label;
     }
-
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setLabel(String label) {
+        this.label = label;
     }
+
     public String getDescription() {
         return description;
     }
-
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Boolean getStatut() {
         return statut;
     }
-
     public void setStatut(Boolean statut) {
         this.statut = statut;
     }
@@ -70,12 +73,14 @@ public class CatalogueDto implements Serializable {
     public void setDatecreation(LocalDate datecreation) {
         this.datecreation = datecreation;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        CatalogueDto entity = (CatalogueDto) o;
+        FamilleDto entity = (FamilleDto) o;
         return Objects.equals(this.id, entity.id) &&
+                Objects.equals(this.idcatalogue, entity.idcatalogue) &&
                 Objects.equals(this.reference, entity.reference) &&
                 Objects.equals(this.label, entity.label) &&
                 Objects.equals(this.description, entity.description) &&
@@ -85,17 +90,28 @@ public class CatalogueDto implements Serializable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, reference, label, description, statut, datecreation);
+        return Objects.hash(id, idcatalogue, reference, label, description, statut, datecreation);
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "(" +
                 "id = " + id + ", " +
+                "idcatalogue = " + idcatalogue + ", " +
                 "reference = " + reference + ", " +
                 "label = " + label + ", " +
                 "description = " + description + ", " +
                 "statut = " + statut + ", " +
                 "datecreation = " + datecreation + ")";
+    }
+
+    public void clear(){
+        this.id = 0;
+        this.idcatalogue = null;
+        this.reference = null;
+        this.label = null;
+        this.description = null;
+        this.statut = true;
+        this.datecreation = null;
     }
 }
